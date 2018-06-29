9be70af8e451d88b6ed7fc9710bb52fe3a066d10
José Gomes <jcarmog@gmail.com>
2018-06-29 17:35:26 -0300

    script carga de cidades. Modelagem da classe aluno

--- server/src/main/java/br/com/company/projetofitness/util/Util.java
+++ server/src/main/java/br/com/company/projetofitness/util/Util.java
@@ -5,16 +5,40 @@
  */
 package br.com.company.projetofitness.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author jcarmo
  */
 public final class Util {
-    
-    public static String removerMascara(String valor){
+
+    public static String removerMascara(String valor) {
         return valor.replaceAll("\\D", "");
     }
+
     public static void main(String[] args) {
-        System.out.println(">>>"+Util.removerMascara("(61)9999-99999"));
+        System.out.println(">>>" + Util.formatarCPFNPJ("01014672147"));
+    }
+
+    public static String formatarCPFNPJ(String valor) {
+        if (valor.length() == 14) {
+            Pattern pattern = Pattern.compile("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})");
+            Matcher matcher = pattern.matcher(valor);
+            if (matcher.find()) {
+                return matcher.replaceAll("$1.$2.$3/$4-$5");
+            }
+            return valor;
+        } else if (valor.length() == 11) {
+            Pattern pattern = Pattern.compile("(\\d{3})(\\d{3})(\\d{3})(\\d{2})");
+            Matcher matcher = pattern.matcher(valor);
+            if (matcher.find()) {
+                return matcher.replaceAll("$1.$2.$3-$4");
+            }
+            return valor;
+        } else {
+            return "";
+        }
     }
 }
