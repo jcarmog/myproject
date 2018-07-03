cdb2ab6f07a56349e81f8ae6a822639009dcb2de
José Gomes <jcarmog@gmail.com>
2018-07-02 22:20:22 -0300

    [BD] scripts de inserção nas tabelas tb_menu, tb_item_menu e tb_perfil

---
+++ scripts/tb_menu.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM dbstronger.tb_menu
+LIMIT 0, 1000
+
+-- Date: 2018-07-02 22:18
+*/
+INSERT INTO `tb_menu` (`id`,`nome`,`ordemExibicao`) VALUES (1,'Cadastros',0);
+INSERT INTO `tb_menu` (`id`,`nome`,`ordemExibicao`) VALUES (2,'Segurança',1);
---
+++ scripts/tb_perfil.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM dbstronger.tb_perfil
+LIMIT 0, 1000
+
+-- Date: 2018-07-02 22:18
+*/
+INSERT INTO `tb_perfil` (`id`,`descricao`) VALUES (1,'Administrador');
